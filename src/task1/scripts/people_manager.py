d461bf11d55772ad3a85490e7493df5766f512e5
Mari-Ice <marija.absec@gmail.com>
2024-03-21 14:03:52 +0100

    Face detection fixes.

--- src/task1/scripts/people_manager.py
+++ src/task1/scripts/people_manager.py
@@ -12,7 +12,7 @@ from visualization_msgs.msg import Marker
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
-
+import math
 
 
 # from rclpy.parameter import Parameter
@@ -23,33 +23,37 @@ class Face():
         self.x = marker.pose.position.x
         self.y = marker.pose.position.y
         self.z = marker.pose.position.z
+
+        self.tresh_xy = 0.1
+        self.tresh_z = 0.02
+
         self.num = 1
 
-    def compare(face):
-        if(math.abs(self.x - face.x) < 1 and math.abs(self.y - face.y) < 1 and math.abs(self.z - face.z) < 1):
+    def compare(self, face):
+        if(math.fabs(self.x - face.x) < self.tresh_xy and math.fabs(self.y - face.y) < self.tresh_xy and math.fabs(self.z - face.z) < self.tresh_z):
             return True
         return False
 
 
 class detect_faces(Node):
 
-	def __init__(self):
-		super().__init__('people_manager')
+    def __init__(self):
+        super().__init__('people_manager')
 
-		self.declare_parameters(
-			namespace='',
-			parameters=[
-				('device', ''),
-		])
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('device', ''),
+        ])
         
         self.faces = []
 
-		marker_topic = "/people_marker"
+        marker_topic = "/people_marker"
 
-		self.marker = self.create_subscription(Marker, marker_topic, self.marker_callback)
-		self.get_logger().info(f"Node has been initialized! Reading from {marker_topic}.")
+        self.marker = self.create_subscription(Marker, marker_topic, self.marker_callback, 10)
+        self.get_logger().info(f"Node has been initialized! Reading from {marker_topic}.")
 
-    def marker_callback(marker):
+    def marker_callback(self, marker):
         new_face = Face(marker)
         notFound = True
         for face in self.faces:
@@ -63,17 +67,17 @@ class detect_faces(Node):
         if(notFound):
             self.faces.append(new_face)
         
-        self.get_logger().info(f"Got a marker {marker.pose.position.x} {marker.pose.posititon.y} {marker.pose.position.z}")
+        self.get_logger().info(f"Got a marker {marker.pose.position.x} {marker.pose.position.y} {marker.pose.position.z}")
         self.get_logger().info(f"FACES: {len(self.faces)}")
 
 def main():
-	print('People manager node starting.')
+    print('People manager node starting.')
 
-	rclpy.init(args=None)
-	node = detect_faces()
-	rclpy.spin(node)
-	node.destroy_node()
-	rclpy.shutdown()
+    rclpy.init(args=None)
+    node = detect_faces()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-	main()
+    main()
