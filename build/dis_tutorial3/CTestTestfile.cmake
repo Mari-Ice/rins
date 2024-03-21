c495fda35d02031d04e1fe22124c2b048fc17fff
Mari-Ice <marija.absec@gmail.com>
2024-03-21 10:54:47 +0100

    initial commit

---
+++ build/dis_tutorial3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/theta/colcon_ws/src/dis_tutorial3
+# Build directory: /home/theta/colcon_ws/build/dis_tutorial3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/dis_tutorial4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/theta/colcon_ws/src/dis_tutorial4
+# Build directory: /home/theta/colcon_ws/build/dis_tutorial4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
