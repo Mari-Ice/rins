0ff4ee4506af55d0df42c91541771e1359c5aa6f
benjamin-lipnik <beni.lipnik@gmail.com>
2024-04-21 03:12:43 +0200

    Ring detection dela se boljse.

--- src/dis_tutorial6/scripts/detect_rings2.py
+++ src/dis_tutorial6/scripts/detect_rings2.py
@@ -34,6 +34,7 @@ class sky_mask(Node):
 
 		self.rgb_sub = message_filters.Subscriber(self, Image, "/oakd/rgb/preview/image_raw")
 		self.pc_sub  = message_filters.Subscriber(self, PointCloud2, "/oakd/rgb/preview/depth/points")
+
 		self.ts = message_filters.ApproximateTimeSynchronizer( [self.rgb_sub, self.pc_sub], 10, 0.3, allow_headerless=False) 
 		self.ts.registerCallback(self.rgb_pc_callback)
 
@@ -48,25 +49,14 @@ class sky_mask(Node):
 		cv2.moveWindow('FatMask',830 ,1)
 		cv2.moveWindow('Circle', 1245,1)
 
-		# cv2.createTrackbar('A', "Image", 0, 100, self.nothing)
-		# cv2.createTrackbar('B', "Image", 0, 100, self.nothing)
-
 	def nothing(self, data):
 		pass
 
 	def rgb_pc_callback(self, rgb, pc):
 		img = self.bridge.imgmsg_to_cv2(rgb, "bgr8")
-
-		A = max(1,cv2.getTrackbarPos('A', 'Image'))
-		B = max(1,cv2.getTrackbarPos('B', 'Image'))
-
-		if((A % 2) == 0):
-			A+=1
-		if((B % 2) == 0):
-			B+=1
-
-		height     = pc.height
-		width      = pc.width
+		img_display = img.copy()
+		height	 = pc.height
+		width	  = pc.width
 		point_step = pc.point_step
 		row_step   = pc.row_step		
 
@@ -74,50 +64,39 @@ class sky_mask(Node):
 		a = a.reshape((height,width,3))
 		hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-		# Tole se zna pridet prav, ...
-		# qube_mask = np.full((height, width), 0, dtype=np.uint8)
-		# qube_mask[(hsv_img[:,:,1] > 35) & (hsv_img[:,:,2] > 35)] = 255
-		# qube_mask[a[:,:,2] < 0.4] = 0
-		# kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (A,B))
-		# qube_mask = cv2.morphologyEx(qube_mask, cv2.MORPH_OPEN, kernel1)
-		# cv2.imshow("QubeMask", qube_mask)
-		# img[qube_mask==255] = (179,179,179)
-		# hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 		mask = np.full((height, width), 0, dtype=np.uint8)
-		mask[(hsv_img[:,:,1] > 35) & (hsv_img[:,:,2] > 35)] = 255
-		mask[a[:,:,2] < 0.4] = 0
+		mask[(a[:,:,2] >= 0.4) & (a[:,:,2] < 1000)] = 255
+		cv2.imshow("Mask", mask)
+	
+		cv2.floodFill(mask, None, (0,height-1), 255) #slow af mogoce poprait, da dela na manjsih kvadrathik.
+		mask = 255-mask
 
-		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-		fat_mask = cv2.dilate(mask, kernel)
-		
-		contours, hierarchy = cv2.findContours(image=fat_mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+		mask = cv2.dilate(mask, kernel)
+		mask[a[:,:,2] > 1000] = 0
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+		mask = cv2.dilate(mask, kernel)
+
+		contours, hierarchy = cv2.findContours(image=mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
 
 		for c in contours:
 			x,y,w,h = cv2.boundingRect(c)
-			x+=2
-			y+=2
-			w-=4
-			h-=4
-
-			cx = int(x + w/2)
-			cy = int(y + h/2)
-
-			if(w < 8 or h < 8):
+			if(w <= 4 or h <= 4):
 				continue
 
-			# pet zarkov, za vecjo stabilnost
-			r0 = sum(a[cy,cx])
-			r1 = sum(a[cy+1,cx])
-			r2 = sum(a[cy,cx+1])
-			r3 = sum(a[cy-1,cx])
-			r4 = sum(a[cy,cx-1])
+			x1 = max(0,x-0)
+			y1 = max(0,y-0)
+				
+			x2 = min(width , x+w+0)
+			y2 = min(height, y+h+0)
 
-			if(min(r0,r1,r2,r3,r4) < 10000):
-				continue
+			cx = int((x1 + x2)/2)
+			cy = int((y1 + y2)/2)
 
-			circle_img = img[y:y+h,x:x+w]
-			cv2.imshow("Circle", circle_img)
+			circle_img  = img[y1:y2,x1:x2].copy()
+			circle_mask = mask[y1:y2,x1:x2]
+			circle_img[circle_mask==0] = (0,0,0)
+			cv2.imshow(f"Circle", circle_img)
 
 			color = circle_img.sum(axis=(0,1))
 			color -= min(color)
@@ -138,12 +117,11 @@ class sky_mask(Node):
 			color_name = color_names[color_index]
 			# print(f"color: {color_name}")
 
-			cv2.rectangle(img, (x, y), (x+w, y+h), color, 1)
-			cv2.putText(img, color_name, (cx ,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+			cv2.rectangle(img_display, (x1, y1), (x2, y2), color, 1)
+			cv2.putText(img_display, color_name, (cx ,cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
 
-		cv2.imshow("Image", img)
-		cv2.imshow("Mask", mask)
-		cv2.imshow("FatMask", fat_mask)
+		cv2.imshow("Image", img_display)
+		cv2.imshow("FatMask", mask)
 		cv2.waitKey(1)
 
 def main():
