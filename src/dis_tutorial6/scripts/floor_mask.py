d9d3881abff8ead88bd1e0efd339653de647fb4c
benjamin-lipnik <beni.lipnik@gmail.com>
2024-04-30 00:21:25 +0200

    Najdeno sredisce parkinga...

--- src/dis_tutorial6/scripts/floor_mask.py
+++ src/dis_tutorial6/scripts/floor_mask.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import rclpy
 import cv2
 import numpy as np
@@ -32,6 +33,10 @@ class sky_mask(Node):
 		self.bridge = CvBridge()
 		self.scan = None
 
+		# For listening and loading the TF
+		self.tf_buffer = Buffer()
+		self.tf_listener = TransformListener(self.tf_buffer, self)
+
 		self.rgb_sub = message_filters.Subscriber(self, Image, "/top_camera/rgb/preview/image_raw")
 		self.pc_sub  = message_filters.Subscriber(self, PointCloud2, "/top_camera/rgb/preview/depth/points")
 		#self.depth_sub = message_filters.Subscriber(self, Image, "/top_camera/rgb/preview/depth")
@@ -47,26 +52,30 @@ class sky_mask(Node):
 		cv2.moveWindow('Mask',   415 ,1)
 
 		cv2.createTrackbar('A', "Image", 0, 1000, self.nothing)
+		cv2.setTrackbarPos("A", "Image", 133)
 		
 	def nothing(self, data):
 		pass
 
-	def draw_depth(self, data):
-		try:
-			depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
-		except CvBridgeError as e:
-			print(e)
-
-		depth_image[depth_image==np.inf] = 0
+	def fit_circle(self, points): 
+		#btw mi sicer vemo, da more met krog radij 0.25
+		#amapak je dosti tezje resit tak sistem heh
+		if(len(points) < 3):
+			return None
 		
-		# Do the necessairy conversion so we can visuzalize it in OpenCV
-		image_1 = depth_image / 65536.0 * 255
-		image_1 = image_1/np.max(image_1)*255
+		mat_A = points.copy()
+		mat_A[:,2] = 1
+		vec_B = mat_A[:,0]**2 + mat_A[:,1]**2
+		
+		#Resimo linearen sistem	 A*[a,b,c]^T=b
+		a,b,c = np.linalg.lstsq(mat_A, vec_B)[0]
 
-		image_viz = np.array(image_1, dtype= np.uint8)
+		cx = a/2
+		cy = b/2
+		r = math.sqrt(4*c+a*a+b*b)/2
 
-		cv2.imshow("Depth", image_viz)
-
+		return [cx,cy,r]
+		
 	#def rgb_pc_callback(self, rgb, pc, depth_raw):
 	def rgb_pc_callback(self, rgb, pc):
 		cv2.waitKey(1)
@@ -76,17 +85,40 @@ class sky_mask(Node):
 		point_step = pc.point_step
 		row_step   = pc.row_step		
 
-		a = pc2.read_points_numpy(pc, field_names= ("x", "y", "z"))
-		a = a.reshape((height,width,3))
+		## Tocke bi lahko transformiral vsaj v koordiante na oakd, zato, da ko se roka premakne, stvari se zmeraj isto delajo ...
+		## Amapak ker zgleda da v pythonovem api-ji ni fukncije ki bi tranformirala cel point cloud, tega potem ne bom delal.
+
+		# #tocke transformiramo v globalne (map) koordinate
+		# time_now = rclpy.time.Time()
+		# timeout = Duration(seconds=10.0)
+		# trans = self.tf_buffer.lookup_transform("map", "top_camera", time_now, timeout)	
+		# pc = do_transform_cloud(pc, trans)
+
+		xyz = pc2.read_points_numpy(pc, field_names= ("y", "z", "x"))
+		xyz = xyz.reshape((height,width,3))
 
 		thresh = 5*cv2.getTrackbarPos("A", 'Image') / 1000
-
 		mask = np.full((height, width), 0, dtype=np.uint8)
 		mask[(img[:,:,:] < 10).all(axis=2)] = 255
-		mask[a[:,:,0] < thresh] = (0)
+		mask[xyz[:,:,2] < thresh] = (0)
 
-		cv2.imshow("Image", img)
+		circle = self.fit_circle(xyz[mask==255])
+		if(circle != None):	
+			circle[1] += 0.20
+			print(circle)
+
+		#naceloma ko enkrat mas krog, se obrnes, tako, da je y koordianta pozitivna. Tj. Najprej dolocis kot in se med obracanjem ne oziras ali se krog vidis ali ne.
+		#razen, ce ga vidis boljse od prej, potem lahko malo popravis kot, do katerega se mores obrant ...
+
+		#Ko se enkrat obrnes tako, zracunas zracunas razdaljo koliko dales se mores premaknati naprej
+		#Podobno kot prej, se tu premaknes naprej za to razdaljo. Tu naceloma ne mores vec zgubit kroga, ...
+
+		#print(xyz[mask==255])
+		# #print(len(xyz[mask==255]))
+		# #print((xyz[mask==255]))
+
 		cv2.imshow("Mask", mask)
+		cv2.imshow("Image", img)
 
 def main():
 	print("OK")
