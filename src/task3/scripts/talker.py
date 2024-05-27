a9f50269d3eb1b20c41d53d3ac144e3578716c4d
Mari-Ice <marija.absec@gmail.com>
2024-05-27 14:01:27 +0200

    talker redone - language processing added

--- src/task3/scripts/talker.py
+++ src/task3/scripts/talker.py
@@ -11,37 +11,34 @@ from pydub.playback import play
 from task2.srv import Color
 from std_srvs.srv import Trigger
 import speech_recognition as sr
+from gtts import gTTs
+import io
+from enum import Enum
+from task3.msg import Park
 
 class Talker(Node):
 	
 	def __init__(self):
 		super().__init__('talker')
 	   
-	   	
-		pwd = os.getcwd()
-		gpath = pwd[0:len(pwd.lower().split("rins")[0])+4]
-		# reconfigure here to have all the talking in this node
-		default_folder = f"{gpath}/src/task2/sounds/"
-
-
-		self.color_files = [
-			'red.mp3', 'green.mp3', 'blue.mp3', 'black.mp3'
-		]
-		self.greet = 'obi-wan-hello-there.mp3'
-		self.declare_parameter('sounds_folder', default_folder)
-	
 		self.srv_color = self.create_service(Color, 'say_color', self.say_color_callback)
 		self.srv_greet = self.create_service(Trigger, 'say_hello', self.say_hello_callback)
 		
+		self.colors = ['red', 'green', 'blue', 'black']
+		self.greet = 'Hello!'
+		self.ask = 'Do you know where the Mona Lisa painting is?'
+		self.clue = 'Do you know where I should look for it?'
+		self.goodbye = 'Thank you very much. Goodbye!'
+
 		self.srv_listen = self.create_service(Trigger, 'listen', self.listen_callback)
+		self.pub_park = self.create_publisher(Park, '/park_near_obj')
 		self.recogniser = sr.Recognizer()
-		self.source = sr.Microphone()
 
 	def say_color_callback(self, request, response):
 		try:
-			color = request.color
+			color = self.colors[request.color]
 			response.color = color
-			response = self.play_sound(self.color_files[color], response)
+			response = self.play_sound(color, response)
 		except Exception as e:
 			response.success = False
 			response.message = str(e)
@@ -54,19 +51,23 @@ class Talker(Node):
 		except Exception as e:
 			response.success = False
 			response.message = str(e)
+			return response
 		finally:
+			response = self.listen_callback(request, response)
 			return response
 
-	def play_sound(self, sound_file, response):
+	def play_sound(self, text, response):
 		try:
-			self.get_logger().info(f'Playing sound {sound_file}')
-
-			sound_file = self.get_parameter('sounds_folder').get_parameter_value().string_value + sound_file
-			sound = AudioSegment.from_file(sound_file)
+			self.get_logger().info(f'Playing sound: {text}')
+			tts = gTTs(text, lang='en')
+			speech_file = io.BytesIO()
+			tts.write_to_fp(speech_file)
+			speech_file.seek(0)
+			sound = AudioSegment.from_file(speech_file, format='mp3')
 			play(sound)
 			
 			response.success = True
-			response.message = sound_file
+			response.message = text
 		except Exception as e:
 			response.success = False
 			response.message = str(e)
@@ -75,26 +76,56 @@ class Talker(Node):
 
 	def listen_callback(self, request, response):
 		understood = False
-		while not understood:
-			print("Say something!")
-			audio = self.recogniser.listen(self.source)
-			# recognize speech using GoogleAPI
-			try:
-				text = self.recogniser.recognize_google(audio)
-				print("[GOOGLE]: " + text)
-				understood = True
-				response.success = True
-				response.message = text
-			except sr.UnknownValueError:
-				print("[GOOGLE]: Could not understand audio")
-				response.success = False
-				response.message = 'Error'
-			except sr.RequestError as e:
-				print("[GOOGLE ERROR]; {0}".format(e))
-				response.success = False
-				response.message = e
+		with sr.Microphone() as source:
+			while not understood:
+				print("Say something!")
+				audio = self.recogniser.listen(source)
+				# recognize speech using GoogleAPI
+				try:
+					text = self.recogniser.recognize_google(audio)
+					print("[GOOGLE]: " + text)
+					understood = True
+					response.message = self.process_text(text, response)
+				except sr.UnknownValueError:
+					print("[GOOGLE]: Could not understand audio")
+					response.success = False
+					response.message = 'Error'
+				except sr.RequestError as e:
+					print("[GOOGLE ERROR]; {0}".format(e))
+					response.success = False
+					response.message = e
 		return response
 	
+	def process_text(self, text, response):
+		# the function for text processing
+		colors = []
+		object = ''
+		clue = False
+		if 'No' in text or 'don\'t' in text:
+			pass
+		else:
+			for color in self.colors:
+				if color in text:
+					colors.append(color)
+			if 'ring' in text:
+				object = 'ring'
+			elif 'cylinder' in text:
+				object = 'cylinder'
+			else:
+				response.success = False
+				return 'Could not understand.'
+			clue = True
+		self.play_sound(self.goodbye)
+		if clue:
+			park = Park()
+			park.color = color
+			park.obj = object
+			self.pub_park.publish(park)
+		response.success = True
+		return 'OK'
+
+	
+
 def main(args=None):
 	print('Talker node starting.')
 
