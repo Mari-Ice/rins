629761e28a2d969f003b6d7151c6caf341b4b9c3
benjamin-lipnik <beni.lipnik@gmail.com>
2024-05-27 02:05:00 +0200

    New training set with perspective fixed. live PCA test (run Task1/face_extractory.py).

--- src/task1/scripts/face_extractor.py
+++ src/task1/scripts/face_extractor.py
@@ -31,6 +31,45 @@ from ultralytics import YOLO
 import sys
 import os
 
+from sklearn.decomposition import PCA
+
+def open_img(path, dtype=np.float64, resize=False, n=120, m=80):
+	image = cv2.imread(path)
+	#image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#print(image.shape)
+	if(resize):
+		image = cv2.resize(image, (m, n))
+	image = np.asarray(image)
+	if(dtype == np.float64):
+		image = image.astype(dtype) / 255
+	return image
+	
+
+def prepare_data(dir, resize=False, n=120, m=80):
+	files = os.listdir(dir)
+	matrix = []
+   
+	for file in files:
+		im = open_img(os.path.join(dir, file), resize=resize, n=n, m=m)
+		#print(im.shape)
+		im = np.reshape(im, -1)
+		#print(im.shape)
+		matrix.append(im)
+
+	return np.array(matrix)
+
+def fit_pca(matrix, n_components=10):
+	pca = PCA(n_components=n_components)
+	pca.fit(matrix)
+	return pca
+
+def get_error(img, pca):
+	original_shape = img.shape
+	img = np.reshape(img, -1)
+	img2 = pca.inverse_transform(pca.transform([img.copy()]))
+	err = abs(img - img2)
+	return np.reshape(err, original_shape)
+
 class face_extractor(Node):
 	def __init__(self):
 		super().__init__('detect_faces')
@@ -52,6 +91,7 @@ class face_extractor(Node):
 
 		self.model = YOLO("yolov8n.pt")
 		cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+		cv2.namedWindow("ErrorMona", cv2.WINDOW_NORMAL)
 
 		self.face_count = 0
 		self.prev_img_stats = [0,0,0,0]
@@ -59,6 +99,12 @@ class face_extractor(Node):
 		pwd = os.getcwd()
 		self.gpath = pwd[0:len(pwd.lower().split("rins")[0])+4]
 
+		n = 300
+		m = 200
+		
+		train_folder = f"{self.gpath}/mona_images"
+		matrix = prepare_data(train_folder, resize=True, n=n, m=m)
+		self.pca = fit_pca(matrix, n_components=50)
 
 	def rgb_callback(self, rgb_data):
 		cv_image = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
@@ -68,50 +114,17 @@ class face_extractor(Node):
 		edge_img[hsv_image[:,:,1] < 200] = (255,255,255)
 		edge_img[(hsv_image[:,:,0] > 30) & (hsv_image[:,:,0] < 220)] = (255,255,255)
 		gray = 255 - cv2.cvtColor(edge_img, cv2.COLOR_BGR2GRAY)
+	
+		#dilate
+		
+		dil_mat = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+		gray = cv2.erode(gray, dil_mat)
+		#cv2.imshow("GrayDilated", gray)
+		
 		contours_tupled, hierarchy = cv2.findContours(image=gray, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
 
 		contours = []
 		for c in contours_tupled:
-			c = cv2.convexHull(c)
-
-			x_coords = c[:,:,0]
-			y_coords = c[:,:,1]
-			min_x_index = np.argmin(x_coords)
-			max_x_index = np.argmax(x_coords)
-
-			#CE y pri mx > kot y pri Mx potem dodas minx
-			#sicer dodas max
-
-			minx = int(x_coords[min_x_index])
-			maxx = int(x_coords[max_x_index])
-			center = 0.5*(minx+maxx)	
-		
-			left_block = []
-			right_block = []
-
-			for i,p in enumerate(c):
-				p = p[0]
-				if(p[0] <= center):
-					left_block.append(p[1])	
-				if(p[0] >= center):
-					right_block.append(p[1])
-
-			minx_y = max(left_block)
-			maxx_y = max(right_block)
-			
-			append_pts = []
-			for p in c:
-				p = p[0]
-				if(p[1] == 0): #Touches top
-					if(minx_y < maxx_y):
-						append_pts.append( [maxx,0] )
-					else:
-						append_pts.append( [minx,0] )
-						
-			for p in append_pts:
-				c = np.vstack((c, np.array([[p]])))
-
-			c = cv2.convexHull(c)
 			contours.append(c)
 
 		mask = np.zeros_like(gray)
@@ -120,11 +133,7 @@ class face_extractor(Node):
 			
 		masked = cv_image.copy()
 		masked[mask == 0] = (255,255,255)
-		#cv2.imshow("Masked", masked)
-
-		# for c in contours:
-		# 	c_box = cv2.boundingRect(c)	
-		# 	cv_image = cv2.rectangle(cv_image, (c_box[0], c_box[1]), (c_box[0] + c_box[2], c_box[1] + c_box[3]), (0,255,0), 2)
+		cv2.imshow("Masked", masked)
 
 		res = self.model.predict(cv_image, imgsz=(256, 320), show=False, verbose=False, classes=[0], device=self.device)
 		for x in res:
@@ -134,7 +143,11 @@ class face_extractor(Node):
 			bbox = bbox[0]
 			if(abs(bbox[2] - bbox[0]) < 50 or abs(bbox[3] - bbox[1]) < 80): #Too small
 				continue
-		   
+		 
+		 	
+			if(abs(0.66 - (abs(bbox[2] - bbox[0]) / abs(bbox[3] - bbox[1]))) > 0.1): #Not proper proportions
+				continue
+
 			img_stats = [int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])]
 			pis = self.prev_img_stats
 			diff = abs(img_stats[0] - pis[0]) + abs(img_stats[1] - pis[1]) + abs(img_stats[2] - pis[2]) + abs(img_stats[3] - pis[3])
@@ -151,24 +164,87 @@ class face_extractor(Node):
 			
 			for i,c in enumerate(contours):
 				c_box = cv2.boundingRect(c)	
+			
 				c_box = [c_box[0], c_box[1], c_box[0] + c_box[2], c_box[1] + c_box[3]]
 				c_surface = abs(c_box[2] - c_box[0]) * abs(c_box[3] - c_box[1])
-				error = abs(c_surface - img_surface)
+				error = 1.0 - (img_surface / c_surface)
 				if(error < min_err):
 					min_err = error
 					min_index = i
 					cbox = [int(c_box[0]), int(c_box[1]), int(c_box[2]), int(c_box[3])]
-				#cv_image = cv2.rectangle(cv_image, (c_box[0], c_box[1]), (c_box[2], c_box[3]), (0,255,0), 2)
+				cv_image = cv2.rectangle(cv_image, (c_box[0], c_box[1]), (c_box[2], c_box[3]), (0,255,0), 2)
 		
-			print(min_err)
-			if(min_err < 10000):
-			 	#cv_image = cv2.rectangle(cv_image, (cbox[0], cbox[1]), (cbox[2], cbox[3]), (255,0,0), 2)
+			# print(min_err)
+			if(min_err < 0.5 and min_err >= 0):
+				cv_image = cv2.rectangle(cv_image, (cbox[0], cbox[1]), (cbox[2], cbox[3]), (255,0,0), 2)
 				mona_img = masked[int(cbox[1]):int(cbox[3]), int(cbox[0]):int(cbox[2])]
 
 				if(diff > 1):
 					self.face_count += 1
-					cv2.imwrite(f"{self.gpath}/mona_images/mona_{self.face_count:04}.jpg", mona_img)
+					#cv2.imwrite(f"{self.gpath}/mona_images/mona_{self.face_count:04}.jpg", mona_img)
 					cv2.imshow("Mona", mona_img)
+
+					#Popravimo perspektivo.
+					#print(mona_img.shape)
+				
+					first_red_left = 0
+					first_red_right = 0
+
+					last_red_left = mona_img.shape[0]
+					last_red_right = mona_img.shape[0]
+				
+					fnd = 0
+					for j in range(mona_img.shape[0]):
+						col_left = mona_img[j, 2][0]
+						col_right = mona_img[j, mona_img.shape[1]-3][0]
+						if((fnd & 1) == 0 and col_left != 255):
+							fnd = fnd | 1
+							first_red_left = j
+					
+						if((fnd & 2) == 0 and col_right != 255):
+							fnd = fnd | 2
+							first_red_right = j
+
+						if(fnd == 3):
+							break
+
+					fnd = 0
+					for j in range(mona_img.shape[0]-1, 0, -1):
+						col_left = mona_img[j, 2][0]
+						col_right = mona_img[j, mona_img.shape[1]-3][0]
+						if((fnd & 1) == 0 and col_left != 255):
+							fnd = fnd | 1
+							last_red_left = j
+					
+						if((fnd & 2) == 0 and col_right != 255):
+							fnd = fnd | 2
+							last_red_right = j
+
+						if(fnd == 3):
+							break
+
+					p0 = [0, first_red_left]
+					p1 = [0, last_red_left]
+					p2 = [mona_img.shape[1], last_red_right]
+					p3 = [mona_img.shape[1], first_red_right]
+					p = np.float32([p0, p1, p2, p3])
+					
+					t0 = [0, 0]
+					t1 = [0, 300]
+					t2 = [200, 300]
+					t3 = [200, 0]
+					t = np.float32([t0, t1, t2, t3])
+				
+					pm = cv2.getPerspectiveTransform(p, t)
+					mona2 = cv2.warpPerspective(mona_img,pm,[200,300])
+					cv2.imshow("Mona2", mona2)
+	
+					mona2 = mona2.astype(np.float64) / 255
+
+					# cv2.imwrite(f"{self.gpath}/mona_images/mona_{self.face_count:04}.jpg", mona2)
+					err = get_error(mona2, self.pca)
+					cv2.imshow("ErrorMona", err) 
+
 
 		cv2.imshow("Image", cv_image)
 		key = cv2.waitKey(1)
--- src/task3/scripts/pca_mona.py
+++ src/task3/scripts/pca_mona.py
@@ -4,70 +4,77 @@ import os
 from sklearn.decomposition import PCA
 
 def open(path, dtype=np.float64, resize=False, n=120, m=80):
-    image = cv2.imread(path)
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #print(image.shape)
-    if(resize):
-        image = cv2.resize(image, (m, n))
-    image = np.asarray(image)
-    if(dtype == np.float64):
-        image = image.astype(dtype) / 255
-    return image
-    
+	image = cv2.imread(path)
+	#image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#print(image.shape)
+	if(resize):
+		image = cv2.resize(image, (m, n))
+	image = np.asarray(image)
+	if(dtype == np.float64):
+		image = image.astype(dtype) / 255
+	return image
+	
 
 def prepare_data(dir, resize=False, n=120, m=80):
-    files = os.listdir(dir)
-    matrix = []
+	files = os.listdir(dir)
+	matrix = []
    
-    for file in files:
-        im = open(os.path.join(dir, file), resize=resize, n=n, m=m)
-        #print(im.shape)
-        im = np.reshape(im, -1)
-        #print(im.shape)
-        matrix.append(im)
+	for file in files:
+		im = open(os.path.join(dir, file), resize=resize, n=n, m=m)
+		#print(im.shape)
+		im = np.reshape(im, -1)
+		#print(im.shape)
+		matrix.append(im)
 
-    return np.array(matrix)
+	return np.array(matrix)
 
 def fit_pca(matrix, n_components=10):
-    pca = PCA(n_components=n_components)
-    pca.fit(matrix)
-    return pca
+	pca = PCA(n_components=n_components)
+	pca.fit(matrix)
+	return pca
 
 def get_error(img, pca):
-    img = np.reshape(img, -1)
-    img2 = pca.inverse_transform(pca.transform([img.copy()]))
-    
-    return abs(img - img2), img2
+	img = np.reshape(img, -1)
+	img2 = pca.inverse_transform(pca.transform([img.copy()]))
+	
+	return abs(img - img2), img2
 
 
 if __name__ == '__main__':
-    n = 200
-    m = 130
-    train_folder = 'C:\\Users\\marij\\Desktop\\docs\\FAKS\\rins\\mona_images'
-    matrix = prepare_data(train_folder, resize=True, n=n, m=m)
-    #print(matrix.shape)
-    #print(np.any(np.isnan(matrix)))
-    pca = fit_pca(matrix, n_components=200)
-    monas = 'C:\\Users\\marij\\Desktop\\docs\\FAKS\\rins\\src\\dis_tutorial3\\worlds\\task3_meshes'
-    monas_fake = ['anomona_0.png', 'anomona_1.png', 'anomona_2.png', 'anomona_3.png', 'anomona_4.png', 
-                  'easy_anomona_0.png', 'easy_anomona_1.png', 'easy_anomona_2.png', 'easy_anomona_3.png', 'easy_anomona_4.png', 'mona.png']
-    monas_real = ['mona_0001.jpg', 'mona_0020.jpg', 'mona_0099.jpg', 'mona_0150.jpg', 'mona_0243.jpg', 'mona_0324.jpg','mona_0432.jpg', 'mona_0523.jpg', 'mona_0610.jpg', 'mona_0677.jpg', 'mona_0732.jpg', 'mona_0844.jpg', 'mona_0934.jpg']
-    
-    for mona in monas_fake:
-        img = open(os.path.join(monas, mona), resize=True, n=n, m=m)
-        err, img2 = get_error(img, pca)
-        err = np.reshape(err, (n, m, 3))
-        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-        cv2.imshow('Image', 1 - err)
-        
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    for mona in monas_real:
-        img = open(os.path.join(train_folder, mona), resize=True, n=n, m=m)
-        err, img2 = get_error(img, pca)
-        err = np.reshape(err, (n, m, 3))
-        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-        cv2.imshow('Image', 1 - err)
-        
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+	#n = 200
+	#m = 130
+	n = 300
+	m = 200
+
+	pwd = os.getcwd()
+	gpath = pwd[0:len(pwd.lower().split("rins")[0])+4]
+
+	train_folder = f"{gpath}/mona_images"
+	monas = f"{gpath}/src/dis_tutorial3/worlds/task3_meshes"
+	matrix = prepare_data(train_folder, resize=True, n=n, m=m)
+
+	#print(matrix.shape)
+	#print(np.any(np.isnan(matrix)))
+	pca = fit_pca(matrix, n_components=20)
+
+	monas_fake = ['anomona_0.png', 'anomona_1.png', 'anomona_2.png', 'anomona_3.png', 'anomona_4.png', 
+				  'easy_anomona_0.png', 'easy_anomona_1.png', 'easy_anomona_2.png', 'easy_anomona_3.png', 'easy_anomona_4.png', 'mona.png']
+	monas_real = ['mona_0001.jpg', 'mona_0020.jpg', 'mona_0099.jpg', 'mona_0150.jpg', 'mona_0243.jpg', 'mona_0324.jpg','mona_0432.jpg', 'mona_0523.jpg', 'mona_0610.jpg', 'mona_0677.jpg', 'mona_0732.jpg', 'mona_0844.jpg', 'mona_0934.jpg']
+	
+	for mona in monas_fake:
+		img = open(os.path.join(monas, mona), resize=True, n=n, m=m)
+		err, img2 = get_error(img, pca)
+		err = np.reshape(err, (n, m, 3))
+		cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+		cv2.imshow('Image', err)
+		
+		cv2.waitKey(0)
+	for mona in monas_real:
+		img = open(os.path.join(train_folder, mona), resize=True, n=n, m=m)
+		err, img2 = get_error(img, pca)
+		err = np.reshape(err, (n, m, 3))
+		cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+		cv2.imshow('Image', err)
+		
+		cv2.waitKey(0)
+	cv2.destroyAllWindows()
